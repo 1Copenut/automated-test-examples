753b87302cdf418715a3b1c9182ce3d733d81b52
1Copenut <pierce.trevor@gmail.com>
2024-08-11 17:22:19 -0500

    Added a chart type switch test and assertions.

--- selenium/webdriver-ruby/selenium_chart_test.rb
+++ selenium/webdriver-ruby/selenium_chart_test.rb
@@ -7,7 +7,7 @@ require 'test-unit'
 class SeleniumChartTest < Test::Unit::TestCase
   def setup
     @url = 'https://finance.yahoo.com/quote/CSCO/'
-    @driver = Selenium::WebDriver.for :chrome
+    @driver = Selenium::WebDriver.for :firefox
     action = Selenium::WebDriver::ActionBuilder
 
     @driver.get(@url)
@@ -17,10 +17,26 @@ class SeleniumChartTest < Test::Unit::TestCase
     @wait = Selenium::WebDriver::Wait.new(timeout: 5)
   end
 
-  def test_heading_one
+  def test_page_heading
     heading = @wait.until { @driver.find_element(:css, 'section.container h1') }
-    puts "Found elem: #{heading.text}"
+    puts "Found h1 elem: #{heading.text}"
     assert_equal(heading.text, 'Cisco Systems, Inc. (CSCO)')
+  end
+
+  def test_chart_type_switcher
+    chart_type_button = @wait.until { @driver.find_element(:css, '[aria-label="Chart Type"]') }
+    @driver.action.move_to(chart_type_button)
+    chart_type_button.click
+
+    sleep 3
+
+    line_chart_button = @wait.until { @driver.find_element(:css, '[data-value="line"]') }
+    @driver.action.move_to(line_chart_button)
+    line_chart_button.click
+
+    sleep 3
+
+    assert_equal(line_chart_button.attribute('aria-selected'), 'true')
   end
 
   def teardown
